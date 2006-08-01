d9ca90f9debfeaccc9f9ac5bb861021d719b2777
Roger Leigh <rleigh@debian.org>
2006-08-01 19:18:37 +0000

    * test/Makefile.am (TESTS): Add sbuild-regex. * test/sbuild-regex.cc: New file.  sbuild::regex unit tests. * sbuild/Makefile.am   (sbuild_public_h_sources): Add sbuild-regex.h. * sbuild/sbuild-regex.h: New file.  POSIX extended regular   expression class, based on boost::regex.  It supports input and   output from streams.

---
+++ sbuild/sbuild-regex.h
@@ -0,0 +1,119 @@
+/* Copyright © 2006  Roger Leigh <rleigh@debian.org>
+ *
+ * schroot is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * schroot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ *********************************************************************/
+
+#ifndef SBUILD_REGEX_H
+#define SBUILD_REGEX_H
+
+#include <istream>
+#include <ostream>
+#include <string>
+
+#include <boost/regex.hpp>
+
+namespace sbuild
+{
+
+  /**
+   * POSIX extended regular expression.
+   */
+  class regex : public boost::regex
+  {
+  public:
+    /// The constructor
+    regex ():
+      boost::regex()
+    {}
+
+    /**
+     * The constructor.
+     *
+     * May throw if the regex is invalid.
+     *
+     * @param pattern a regex
+     */
+    regex (std::string const& pattern):
+      boost::regex(pattern, boost::regex::extended)
+    {}
+
+    /**
+     * The constructor.
+     *
+     * May throw if the regex is invalid.
+     *
+     * @param pattern a regex
+     */
+    regex (const char *pattern):
+      boost::regex(pattern, boost::regex::extended)
+    {}
+
+    ///* The destructor.
+    ~regex ()
+    {}
+
+    /**
+     * Get the regex name from a stream.
+     *
+     * May throw if the regex is invalid.
+     *
+     * @param stream the stream to get input from.
+     * @param rhs the regex to set.
+     * @returns the stream.
+     */
+    template <class charT, class traits>
+    friend
+    std::basic_istream<charT,traits>&
+    operator >> (std::basic_istream<charT,traits>& stream,
+		 regex&                            rhs)
+    {
+      std::string regex;
+
+      if (std::getline(stream, regex))
+	{
+	  rhs.assign(regex, boost::regex::extended);
+	}
+
+      return stream;
+    }
+
+    /**
+     * Print the regex name to a stream.
+     *
+     * @param stream the stream to output to.
+     * @param rhs the regex to output.
+     * @returns the stream.
+     */
+    template <class charT, class traits>
+    friend
+    std::basic_ostream<charT,traits>&
+    operator << (std::basic_ostream<charT,traits>& stream,
+		 regex const&                rhs)
+    {
+      return stream << rhs.str();
+    }
+  };
+
+}
+
+#endif /* SBUILD_REGEX_H */
+
+/*
+ * Local Variables:
+ * mode:C++
+ * End:
+ */
---
+++ test/sbuild-regex.cc
@@ -0,0 +1,91 @@
+/* Copyright © 2006  Roger Leigh <rleigh@debian.org>
+ *
+ * schroot is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * schroot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ *
+ *********************************************************************/
+
+#include <sbuild/sbuild-regex.h>
+
+#include <iostream>
+#include <sstream>
+
+#include <cppunit/extensions/HelperMacros.h>
+
+using namespace CppUnit;
+
+class test_regex : public TestCase
+{
+  CPPUNIT_TEST_SUITE(test_regex);
+  CPPUNIT_TEST(test_construction);
+  CPPUNIT_TEST_EXCEPTION(test_construction_fail, boost::regex_error);
+  CPPUNIT_TEST(test_output);
+  CPPUNIT_TEST(test_input);
+  CPPUNIT_TEST_EXCEPTION(test_input_fail, boost::regex_error);
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  test_regex()
+  {}
+
+  virtual ~test_regex()
+  {}
+
+  void
+  test_construction()
+  {
+    sbuild::regex r1;
+
+    sbuild::regex r2("foo");
+
+    std::string p("foo|bar$");
+    sbuild::regex r3(p);
+  }
+
+  void
+  test_construction_fail()
+  {
+    sbuild::regex r("[foo");
+  }
+
+  void
+  test_output()
+  {
+    sbuild::regex r("foo");
+    std::ostringstream o;
+    o << r;
+    CPPUNIT_ASSERT(o.str() == "foo");
+  }
+
+  void
+  test_input()
+  {
+    sbuild::regex r;
+    std::istringstream i("foo");
+    i >> r;
+    CPPUNIT_ASSERT(r.str() == "foo");
+  }
+
+  void
+  test_input_fail()
+  {
+    sbuild::regex r;
+    std::istringstream i("([[invalid_regex");
+    i >> r;
+  }
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(test_regex);
