736f6bb57af60320e656113ea593886336619435
Roger Leigh <rleigh@debian.org>
2008-01-08 00:29:34 +0000

    * po/POTFILES.in: Add bin/schroot-mount sources. * configure.ac: Output bin/schroot-mount/Makefile * bin/Makefile.am (SUBDIRS): Add schroot-mount. * bin/schroot-mount: Copy schroot-listmounts and search and   replace listmounts with mount.  This is the starting mount for the   schroot-mount utility to parse fstab(5) format files for mounting   filessystems inside chroots.

---
+++ bin/schroot-mount/schroot-mount-main.h
@@ -0,0 +1,97 @@
+/* Copyright © 2005-2007  Roger Leigh <rleigh@debian.org>
+ *
+ * schroot is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * schroot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *********************************************************************/
+
+#ifndef SCHROOT_MOUNT_MAIN_H
+#define SCHROOT_MOUNT_MAIN_H
+
+#include <schroot-base/schroot-base-main.h>
+
+#include <schroot-mount/schroot-mount-options.h>
+
+#include <sbuild/sbuild-custom-error.h>
+
+namespace schroot_mount
+{
+
+  /**
+   * Frontend for schroot-mount.  This class is used to "run" schroot-mount.
+   */
+  class main : public schroot_base::main
+  {
+  public:
+    /// Error codes.
+    enum error_code
+      {
+	FIND, ///< Failed to find file.
+	OPEN, ///< Failed to open file.
+	CLOSE ///< Failed to close file.
+      };
+
+    /// Exception type.
+    typedef sbuild::custom_error<error_code> error;
+
+    /**
+     * The constructor.
+     *
+     * @param options the command-line options to use.
+     */
+    main (options::ptr& options);
+
+    /// The destructor.
+    virtual ~main ();
+
+  private:
+    /**
+     * List mounts (internal helper).
+     *
+     * @param mountfile the file containing the database of mounted
+     * filesystems.
+     */
+    sbuild::string_list
+    list_mounts (std::string const& mountfile) const;
+
+    /**
+     * List mounts.
+     */
+    virtual void
+    action_mount ();
+
+  protected:
+    /**
+     * Run the program.
+     *
+     * @returns 0 on success, 1 on failure or the exit status of the
+     * chroot command.
+     */
+    virtual int
+    run_impl ();
+
+  private:
+    /// The program options.
+    options::ptr opts;
+  };
+
+}
+
+#endif /* SCHROOT_MOUNT_MAIN_H */
+
+/*
+ * Local Variables:
+ * mode:C++
+ * End:
+ */
---
+++ bin/schroot-mount/schroot-mount.cc
@@ -0,0 +1,47 @@
+/* Copyright © 2005-2007  Roger Leigh <rleigh@debian.org>
+ *
+ * schroot is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * schroot is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *********************************************************************/
+
+#include <config.h>
+
+#include "schroot-mount-options.h"
+#include "schroot-mount-main.h"
+
+#include <schroot-base/schroot-base-run.h>
+
+using std::endl;
+using boost::format;
+namespace opt = boost::program_options;
+
+using namespace schroot_mount;
+
+/**
+ * Main routine.
+ *
+ * @param argc the number of arguments
+ * @param argv argument vector
+ *
+ * @returns 0 on success, 1 on failure or the exit status of the
+ * chroot command.
+ */
+int
+main (int   argc,
+      char *argv[])
+{
+  return schroot_base::run
+    <schroot_mount::options, schroot_mount::main>(argc, argv);
+}
