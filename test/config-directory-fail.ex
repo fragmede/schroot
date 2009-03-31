fb4c96f21bb7d833fde6668d7a66d680082590dd
Jan-Marek Glogowski <glogow@fbihome.de>
2009-03-31 23:50:56 +0100

    [test] Add chroot_directory test data
    Signed-off-by: Roger Leigh <rleigh@debian.org>

---
+++ test/config-directory-fail.ex
@@ -0,0 +1,10 @@
+[fail]
+description=Debian experimental
+directory=/srv/chroot/experimental
+location=/srv/chroot/experimental
+priority=3
+groups=sbuild,root
+root-groups=root,sbuild
+run-setup-scripts=true
+run-exec-scripts=true
+
---
+++ test/config-directory-valid.ex
@@ -0,0 +1,9 @@
+[valid]
+description=Debian experimental
+directory=/srv/chroot/experimental
+priority=3
+groups=sbuild,root
+root-groups=root,sbuild
+run-setup-scripts=true
+run-exec-scripts=true
+
