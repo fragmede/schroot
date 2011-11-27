71721880b979ae41701674e356198652d6457ea7
Roger Leigh <rleigh@debian.org>
2011-11-27 13:55:43 +0000

    build: Move all path specification to global.mk
    It's not possible to evaluate directory variables at configure
    time, even with eval.  Move all preprocessor definitions to
    DEFS in global.mk.
    
    Note that this also required changing the substitution mechanism
    in the manual pages, which have been changed to source a
    generated config.man fragment, which is sourced using the .so
    directive, and the manual page generated using soelim(1).
    The boilerplate for the authors and copyright sections has also
    been split out at the same time.

--- scripts/global.mk
+++ scripts/global.mk
@@ -19,9 +19,38 @@
 #
 #####################################################################
 
+schroot_mountdir=$(localstatedir)/lib/$(PACKAGE)/mount
+schroot_sessiondir=$(localstatedir)/lib/$(PACKAGE)/session
+schroot_file_unpackdir=$(localstatedir)/lib/$(PACKAGE)/unpack
+schroot_overlaydir=$(localstatedir)/lib/$(PACKAGE)/union/overlay
+schroot_underlaydir=$(localstatedir)/lib/$(PACKAGE)/union/underlay
+schroot_sysconfdir=$(sysconfdir)/schroot
+schroot_sysconf_chrootdir=$(schroot_sysconfdir)/chroot.d
+schroot_sysconf_setupdir=$(schroot_sysconfdir)/setup.d
+schroot_setupdatadir=$(pkgdatadir)/setup
+
+SCHROOT_CONF=$(schroot_sysconfdir)/schroot.conf
+DCHROOT_CONF="$(sysconfdir)/dchroot.conf"
+CSBUILD_CONF=$(sysconfdir)/csbuild.conf
+
 # Global options for use in all Makefiles.
 AM_CXXFLAGS = -I$(top_srcdir) $(LOCAL_CXXFLAGS) $(PTHREAD_CFLAGS) -pedantic -Wall -Wcast-align -Wwrite-strings -Wswitch-default -Wcast-qual -Wunused-variable -Wredundant-decls -Wctor-dtor-privacy -Wnon-virtual-dtor -Wreorder -Wold-style-cast -Woverloaded-virtual -fstrict-aliasing
 
 AM_LDFLAGS = $(LOCAL_LDFLAGS) $(PTHREAD_LIBS)
 
-DEFS = $(LOCAL_DEFS) -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE
+# Note, if new definitions are added, also update the manual page
+DEFS = $(LOCAL_DEFS) -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE \
+-DSCHROOT_LIBEXEC_DIR=\"$(pkglibexecdir)\" \
+-DSCHROOT_MOUNT_DIR=\"$(schroot_mountdir)\" \
+-DSCHROOT_SESSION_DIR=\"$(schroot_sessiondir)\" \
+-DSCHROOT_FILE_UNPACK_DIR=\"$(schroot_file_unpackdir)\" \
+-DSCHROOT_OVERLAY_DIR=\"$(schroot_overlaydir)\" \
+-DSCHROOT_UNDERLAY_DIR=\"$(schroot_underlaydir)\" \
+-DSCHROOT_SYSCONF_DIR=\"$(schroot_sysconfdir)\" \
+-DSCHROOT_CONF=\"$(SCHROOT_CONF)\" \
+-DSCHROOT_CONF_CHROOT_D=\"$(schroot_sysconf_chrootdir)\" \
+-DSCHROOT_CONF_SETUP_D=\"$(schroot_sysconf_setupdir)\" \
+-DSCHROOT_SETUP_DATA_DIR=\"$(schroot_setupdatadir)\" \
+-DPACKAGE_LOCALE_DIR=\"$(localedir)\" \
+-DSCHROOT_DATA_DIR=\"$(schroot_datadir)\" \
+-DSCHROOT_MODULE_DIR=\"$(schroot_moduledir)\"
