3c2aaf14dd53e7b04098570cef8f6961405fff94
Roger Leigh <rleigh@debian.org>
2013-09-07 23:57:09 +0100

    test: Convert test/sbuild/parse-value to use gtest

--- test/sbuild/parse-value.cc
+++ test/sbuild/parse-value.cc
@@ -16,101 +16,62 @@
  *
  *********************************************************************/
 
+#include <gtest/gtest.h>
+
 #include <sbuild/parse-value.h>
 
-#include <cppunit/extensions/HelperMacros.h>
-
-using namespace CppUnit;
-
-class test_parse_value : public TestCase
+TEST(ParseValue, Bool)
 {
-  CPPUNIT_TEST_SUITE(test_parse_value);
-  CPPUNIT_TEST(test_bool);
-  CPPUNIT_TEST(test_bool_fail);
-  CPPUNIT_TEST(test_int);
-  CPPUNIT_TEST(test_int_fail);
-  CPPUNIT_TEST(test_string);
-  CPPUNIT_TEST_SUITE_END();
+  bool result;
 
-public:
-  test_parse_value()
-  {}
+  result = false;
+  sbuild::parse_value("true", result);
+  EXPECT_TRUE(result);
+  result = false;
+  sbuild::parse_value("yes", result);
+  EXPECT_TRUE(result);
+  result = false;
+  sbuild::parse_value("1", result);
+  EXPECT_TRUE(result);
 
-  void test_bool()
-  {
-    bool result;
+  result = true;
+  sbuild::parse_value("false", result);
+  EXPECT_FALSE(result);
+  result = true;
+  sbuild::parse_value("no", result);
+  EXPECT_FALSE(result);
+  result = true;
+  sbuild::parse_value("0", result);
+  EXPECT_FALSE(result);
+}
 
-    result = false;
-    sbuild::parse_value("true", result);
-    CPPUNIT_ASSERT(result == true);
-    result = false;
-    sbuild::parse_value("yes", result);
-    CPPUNIT_ASSERT(result == true);
-    result = false;
-    sbuild::parse_value("1", result);
-    CPPUNIT_ASSERT(result == true);
+TEST(ParseValue, BoolFail)
+{
+  bool result = true;
 
-    result = true;
-    sbuild::parse_value("false", result);
-    CPPUNIT_ASSERT(result == false);
-    result = true;
-    sbuild::parse_value("no", result);
-    CPPUNIT_ASSERT(result == false);
-    result = true;
-    sbuild::parse_value("0", result);
-    CPPUNIT_ASSERT(result == false);
-  }
+  EXPECT_THROW(sbuild::parse_value("invalid", result), sbuild::parse_value_error);
+  EXPECT_TRUE(result);
+}
 
-  void test_bool_fail()
-  {
-    bool result = true;
+TEST(ParseValue, Int)
+{
+  int result = 0;
+  sbuild::parse_value("23", result);
+  EXPECT_EQ(result, 23);
+}
 
-    try
-      {
-        sbuild::parse_value("invalid", result);
-      }
-    catch (const sbuild::parse_value_error& e)
-      {
-        // Exception thown, and original value unmodified.
-        CPPUNIT_ASSERT(result == true);
-        return;
-      }
-    // Should never be reached
-    CPPUNIT_ASSERT(false);
-  }
+TEST(ParseValue, IntFail)
+{
+  int result = 22;
 
-  void test_int()
-  {
-    int result = 0;
-    sbuild::parse_value("23", result);
-    CPPUNIT_ASSERT(result == 23);
-  }
+  EXPECT_THROW(sbuild::parse_value("invalid", result), sbuild::parse_value_error);
+  EXPECT_EQ(result, 22);
+}
 
-  void test_int_fail()
-  {
-    int result = 22;
+TEST(ParseValue, String)
+{
+  std::string result;
 
-    try
-      {
-        sbuild::parse_value("invalid", result);
-      }
-    catch (const sbuild::parse_value_error& e)
-      {
-        // Exception thown, and original value unmodified.
-        CPPUNIT_ASSERT(result == 22);
-        return;
-      }
-    // Should never be reached
-    CPPUNIT_ASSERT(false);
-  }
-
-  void test_string()
-  {
-    std::string result;
-
-    sbuild::parse_value("test string", result);
-    CPPUNIT_ASSERT(result == "test string");
-  }
-};
-
-CPPUNIT_TEST_SUITE_REGISTRATION(test_parse_value);
+  sbuild::parse_value("test string", result);
+  EXPECT_EQ(result, "test string");
+}
