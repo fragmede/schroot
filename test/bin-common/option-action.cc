2a7730be0ee929ca117b8e016d35724f3aa67478
Roger Leigh <rleigh@debian.org>
2013-09-08 11:51:44 +0100

    test: Convert test/bin-common/option-action to use gtest

--- test/bin-common/option-action.cc
+++ test/bin-common/option-action.cc
@@ -16,46 +16,29 @@
  *
  *********************************************************************/
 
+#include <gtest/gtest.h>
+
 #include <bin-common/option-action.h>
 #include <bin-common/options.h>
 
 #include <iostream>
 
-#include <cppunit/extensions/HelperMacros.h>
-
 using bin_common::option_action;
-using namespace CppUnit;
 
-class test_option_action : public TestFixture
+
+class OptionAction : public ::testing::Test
 {
-  CPPUNIT_TEST_SUITE(test_option_action);
-  CPPUNIT_TEST(test_construction);
-  CPPUNIT_TEST(test_default);
-  CPPUNIT_TEST_EXCEPTION(test_default_fail, bin_common::options::error);
-  CPPUNIT_TEST(test_current);
-  CPPUNIT_TEST_EXCEPTION(test_current_fail, bin_common::options::error);
-  CPPUNIT_TEST_EXCEPTION(test_current_fail_multipleset,
-                         bin_common::options::error);
-  CPPUNIT_TEST(test_operators);
-  CPPUNIT_TEST_EXCEPTION(test_operators_fail_multipleset,
-                         bin_common::options::error);
-  CPPUNIT_TEST_SUITE_END();
-
-protected:
+public:
   option_action *action;
 
-public:
-  test_option_action():
-    TestFixture(),
-    action()
-  {}
-
-  virtual ~test_option_action()
-  {}
-
-  void setUp()
+  void SetUp()
   {
-    this->action = new option_action;
+    action = new option_action;
+  }
+
+  void TearDown()
+  {
+    delete action;
   }
 
   static void
@@ -67,92 +50,77 @@ public:
     act.add("info");
     act.add("config");
   }
-
-  void tearDown()
-  {
-    delete this->action;
-  }
-
-  void
-  test_construction()
-  {
-    option_action act;
-  }
-
-  void
-  test_default()
-  {
-    add_examples(*this->action);
-    CPPUNIT_ASSERT(this->action->get_default() == "");
-    CPPUNIT_ASSERT(this->action->get() == "");
-
-    this->action->set_default("info");
-    CPPUNIT_ASSERT(this->action->get_default() == "info");
-    CPPUNIT_ASSERT(this->action->get() == "info");
-  }
-
-  void
-  test_default_fail()
-  {
-    add_examples(*this->action);
-
-    this->action->set_default("invalid");
-  }
-
-  void
-  test_current()
-  {
-    add_examples(*this->action);
-
-    CPPUNIT_ASSERT(this->action->get_default() == "");
-    CPPUNIT_ASSERT(this->action->get() == "");
-    this->action->set_default("list");
-
-    CPPUNIT_ASSERT(this->action->get_default() == "list");
-    CPPUNIT_ASSERT(this->action->get() == "list");
-
-    this->action->set("config");
-    CPPUNIT_ASSERT(this->action->get_default() == "list");
-    CPPUNIT_ASSERT(this->action->get() == "config");
-  }
-
-  void
-  test_current_fail()
-  {
-    add_examples(*this->action);
-
-    this->action->set("invalid");
-  }
-
-  void
-  test_current_fail_multipleset()
-  {
-    add_examples(*this->action);
-
-    this->action->set("list");
-    this->action->set("info");
-  }
-
-  void
-  test_operators()
-  {
-    add_examples(*this->action);
-
-    *this->action = "list";
-    CPPUNIT_ASSERT(*this->action == "list");
-    CPPUNIT_ASSERT(!(*this->action != "list"));
-    CPPUNIT_ASSERT(*this->action != "invalid");
-    CPPUNIT_ASSERT(!(*this->action == "invalid"));
-  }
-
-  void
-  test_operators_fail_multipleset()
-  {
-    add_examples(*this->action);
-
-    *this->action = "list";
-    *this->action = "config";
-  }
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(test_option_action);
+TEST_F(OptionAction, Construct)
+{
+  ASSERT_NO_THROW(option_action act);
+}
+
+TEST_F(OptionAction, Default)
+{
+  add_examples(*action);
+  EXPECT_EQ(action->get_default(), "");
+  EXPECT_EQ(action->get(), "");
+
+  action->set_default("info");
+  EXPECT_EQ(action->get_default(), "info");
+  EXPECT_EQ(action->get(), "info");
+}
+
+TEST_F(OptionAction, DefaultFail)
+{
+  add_examples(*action);
+
+  ASSERT_THROW(action->set_default("invalid"), bin_common::options::error);
+}
+
+TEST_F(OptionAction, Current)
+{
+  add_examples(*action);
+
+  EXPECT_EQ(action->get_default(), "");
+  EXPECT_EQ(action->get(), "");
+  action->set_default("list");
+
+  EXPECT_EQ(action->get_default(), "list");
+  EXPECT_EQ(action->get(), "list");
+
+  action->set("config");
+  EXPECT_EQ(action->get_default(), "list");
+  EXPECT_EQ(action->get(), "config");
+}
+
+TEST_F(OptionAction, CurrentFail)
+{
+  add_examples(*action);
+
+  ASSERT_THROW(action->set("invalid"), bin_common::options::error);
+}
+
+TEST_F(OptionAction, CurrentFailMultipleSet)
+{
+  add_examples(*action);
+
+  ASSERT_NO_THROW(action->set("list"));
+  ASSERT_THROW(action->set("info"), bin_common::options::error);
+}
+
+TEST_F(OptionAction, Operators)
+{
+  add_examples(*action);
+
+  *action = "list";
+  EXPECT_TRUE(*action == "list");
+  EXPECT_FALSE(*action !="list");
+  EXPECT_TRUE(*action != "invalid");
+  EXPECT_FALSE(*action == "invalid");
+}
+
+TEST_F(OptionAction, OperatorsFailMultipleSet)
+{
+  add_examples(*action);
+
+  ASSERT_NO_THROW(*action = "list");
+  ASSERT_THROW(*action = "config", bin_common::options::error);
+}
