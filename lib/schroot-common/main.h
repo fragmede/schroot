ef578f8ee870d1734a47e3d3290d2d6c86674d73
Roger Leigh <rleigh@debian.org>
2014-02-08 14:10:28 +0000

    schroot-common: Use bin namespace

--- lib/schroot-common/main.h
+++ lib/schroot-common/main.h
@@ -25,129 +25,132 @@
 #include <sbuild/chroot/config.h>
 #include <sbuild/custom-error.h>
 
-namespace schroot_common
+namespace bin
 {
-
-  /**
-   * Frontend base for schroot programs.  This class is used to "run"
-   * schroot programs.  This class contains functionality common to
-   * all schroot programs (schroot, dchroot, dchroot-dsa).
-   */
-  class main : public bin_common::main
+  namespace schroot_common
   {
-  public:
-    /// Error codes.
-    enum error_code
-      {
-        CHROOT_FILE,       ///< No chroots are defined in ....
-        CHROOT_FILE2,      ///< No chroots are defined in ... or ....
-        CHROOT_NOTDEFINED, ///< The specified chroots are not defined.
-        SESSION_INVALID    ///< Invalid session name.
-      };
-
-    /// Exception type.
-    typedef sbuild::custom_error<error_code> error;
-
-    typedef sbuild::chroot::config::chroot_map chroot_map;
 
     /**
-     * The constructor.
-     *
-     * @param program_name the program name.
-     * @param program_usage the program usage message.
-     * @param options the command-line options to use.
-     * @param use_syslog whether to open a connection to the system
-     * logger.
+     * Frontend base for schroot programs.  This class is used to "run"
+     * schroot programs.  This class contains functionality common to
+     * all schroot programs (schroot, dchroot, dchroot-dsa).
      */
-    main (const std::string&                 program_name,
-          const std::string&                 program_usage,
-          schroot_common::options::ptr&      options,
-          bool               use_syslog);
+    class main : public bin_common::main
+    {
+    public:
+      /// Error codes.
+      enum error_code
+        {
+          CHROOT_FILE,       ///< No chroots are defined in ....
+          CHROOT_FILE2,      ///< No chroots are defined in ... or ....
+          CHROOT_NOTDEFINED, ///< The specified chroots are not defined.
+          SESSION_INVALID    ///< Invalid session name.
+        };
 
-    /// The destructor.
-    virtual ~main ();
+      /// Exception type.
+      typedef sbuild::custom_error<error_code> error;
 
-    virtual void
-    action_version (std::ostream& stream);
+      typedef sbuild::chroot::config::chroot_map chroot_map;
 
-    /**
-     * List chroots.
-     */
-    virtual void
-    action_list () = 0;
+      /**
+       * The constructor.
+       *
+       * @param program_name the program name.
+       * @param program_usage the program usage message.
+       * @param options the command-line options to use.
+       * @param use_syslog whether to open a connection to the system
+       * logger.
+       */
+      main (const std::string&                 program_name,
+            const std::string&                 program_usage,
+            schroot_common::options::ptr&      options,
+            bool               use_syslog);
 
-    /**
-     * Print detailed information about chroots.
-     */
-    virtual void
-    action_info ();
+      /// The destructor.
+      virtual ~main ();
 
-    /**
-     * Print location of chroots.
-     */
-    virtual void
-    action_location ();
+      virtual void
+      action_version (std::ostream& stream);
 
-    /**
-     * Dump configuration file for chroots.
-     */
-    virtual void
-    action_config ();
+      /**
+       * List chroots.
+       */
+      virtual void
+      action_list () = 0;
 
-  protected:
-    /**
-     * Run the program.  This is the program-specific run method which
-     * must be implemented in a derived class.
-     *
-     * @returns 0 on success, 1 on failure or the exit status of the
-     * chroot command.
-     */
-    virtual int
-    run_impl ();
+      /**
+       * Print detailed information about chroots.
+       */
+      virtual void
+      action_info ();
 
-    /**
-     * Get a list of chroots based on the specified options (--all, --chroot).
-     *
-     * @returns a list of chroots.
-     */
-    void
-    get_chroot_options ();
+      /**
+       * Print location of chroots.
+       */
+      virtual void
+      action_location ();
 
-    /**
-     * Load configuration.
-     */
-    virtual void
-    load_config ();
+      /**
+       * Dump configuration file for chroots.
+       */
+      virtual void
+      action_config ();
 
-    /**
-     * Create a session.  This sets the session member.
-     *
-     * @param sess_op the session operation to perform.
-     */
-    virtual void
-    create_session (sbuild::session::operation sess_op) = 0;
+    protected:
+      /**
+       * Run the program.  This is the program-specific run method which
+       * must be implemented in a derived class.
+       *
+       * @returns 0 on success, 1 on failure or the exit status of the
+       * chroot command.
+       */
+      virtual int
+      run_impl ();
 
-    /**
-     * Add PAM authentication handler to the session.
-     */
-    virtual void
-    add_session_auth ();
+      /**
+       * Get a list of chroots based on the specified options (--all, --chroot).
+       *
+       * @returns a list of chroots.
+       */
+      void
+      get_chroot_options ();
 
-  protected:
-    /// The program options.
-    schroot_common::options::ptr options;
-    /// The chroot configuration.
-    sbuild::chroot::config::ptr  config;
-    /// The chroots to use (original names or aliases).
-    sbuild::string_list          chroot_names;
-    /// The chroots to use (alias to chroot mapping).
-    chroot_map                   chroots;
-    /// The chroots to use (for session).
-    sbuild::session::chroot_list chroot_objects;
-    /// The session.
-    sbuild::session::ptr         session;
-  };
+      /**
+       * Load configuration.
+       */
+      virtual void
+      load_config ();
 
+      /**
+       * Create a session.  This sets the session member.
+       *
+       * @param sess_op the session operation to perform.
+       */
+      virtual void
+      create_session (sbuild::session::operation sess_op) = 0;
+
+      /**
+       * Add PAM authentication handler to the session.
+       */
+      virtual void
+      add_session_auth ();
+
+    protected:
+      /// The program options.
+      schroot_common::options::ptr options;
+      /// The chroot configuration.
+      sbuild::chroot::config::ptr  config;
+      /// The chroots to use (original names or aliases).
+      sbuild::string_list          chroot_names;
+      /// The chroots to use (alias to chroot mapping).
+      chroot_map                   chroots;
+      /// The chroots to use (for session).
+      sbuild::session::chroot_list chroot_objects;
+      /// The session.
+      sbuild::session::ptr         session;
+    };
+
+  }
 }
 
 #endif /* SCHROOT_COMMON_MAIN_H */
